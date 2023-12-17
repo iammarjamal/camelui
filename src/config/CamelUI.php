6de951bdaed8958abf50dc9156bb8a652c8fce5d
iammarjamal <ammar.jamal.jadallah@gmail.com>
2023-12-17 05:18:03 +0200

    Update CamelUI.php

--- src/config/CamelUI.php
+++ src/config/CamelUI.php
@@ -1,33 +1,5 @@
 <?php
 
 return [
-    
-    'color' => [
-        'primary' => 'indigo-500',
-
-        'secondary' => '6b7280',
-
-        'success' => '22c55e',
-
-        'danger' => 'ef4444',
-
-        'warning' => 'eab308',
-
-        'info' => '0ea5e9',
-
-        'white' => 'ffffff',
-
-        'black' => '000000',
-    ],
-
-    'style' => [
-
-        'rounded' => 'rounded-md',
-
-        'shadow' => 'shadow-sm',
-
-        ''
-
-    ],
-    
+    //
 ];
