e13439254145299341789e178165a276bc648d9b
iammarjamal <ammar.jamal.jadallah@gmail.com>
2023-12-19 22:47:54 +0200

    Update modal.blade.php

--- src/views/components/modal.blade.php
+++ src/views/components/modal.blade.php
@@ -21,9 +21,7 @@ x-transition:leave-start="opacity-100"
 x-transition:leave-end="opacity-0"
 >
     <div 
-    class="{{ $class }} flex-col flex min-3-xl sm:!border-t-1 sm:!border-0 sm:!bottom-0 md:!border-t-1 md:!border-0 md:!bottom-0 shadow dark:shadow-zinc-800 overflow-y-scroll no-scrollbar dark:bg-zinc-800 border-zinc-300 dark:border-zinc-600 sm:fixed md:fixed lg:relative xl:relative ease-in-out z-50 p-2 bg-zinc-50 border cursor-auto sm:p-4 md:p-6 lg:p-8 xl:p-10 
-    {{ $full ? 'sm:!rounded-b-none sm:!rounded-t-3xl md:!rounded-b-none md:!rounded-t-3xl lg:!rounded-b-none lg:!rounded-t-none xl:!rounded-b-none xl:!rounded-t-none sm:h-[96%] md:h-[96%] lg:h-full xl:h-full w-full' : 'rounded-md sm:!rounded-b-none sm:!rounded-t-3xl md:!rounded-t-3xl md:!rounded-b-none lg:!rounded-b-md xl:!rounded-b-md lg:!rounded-t-md xl:!rounded-t-md sm:max-h-[96%] md:max-h-[96%] lg:max-h-[94%] xl:max-h-[94%] sm:w-full md:w-full lg:w-1/2 xl:w-1/3 max-w-7xl' }}
-    " :class="{{$wire}} ? 'animate__animated animate__fadeInUp animate__faster' : 'animate__animated animate__fadeOutDown animate__faster'"
+    class="{{ $class }} flex-col flex min-3-xl sm:!border-t-1 sm:!border-0 sm:!bottom-0 md:!border-t-1 md:!border-0 md:!bottom-0 shadow dark:shadow-zinc-800 overflow-y-scroll no-scrollbar dark:bg-zinc-800 border-zinc-300 dark:border-zinc-600 sm:fixed md:fixed lg:relative xl:relative ease-in-out z-50 p-2 bg-zinc-50 border cursor-auto sm:p-4 md:p-6 lg:p-8 xl:p-10 rounded-md sm:!rounded-b-none sm:!rounded-t-3xl md:!rounded-t-3xl md:!rounded-b-none lg:!rounded-b-md xl:!rounded-b-md lg:!rounded-t-md xl:!rounded-t-md sm:max-h-[96%] md:max-h-[96%] lg:max-h-[94%] xl:max-h-[94%] sm:w-full md:w-full lg:w-1/2 xl:w-1/3 max-w-7xl" :class="{{$wire}} ? 'animate__animated animate__fadeInUp animate__faster' : 'animate__animated animate__fadeOutDown animate__faster'"
     @if(empty($required)) @click.outside="{{$wire}} = false" @endif
     x-show="{{ $wire }}"
     x-transition:enter="transition-transform duration-300 opacity-100"
