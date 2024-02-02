a953ed71309090be25464dbbe1764acac4aa257c
iammarjamal <ammar.jamal.jadallah@gmail.com>
2024-02-02 21:06:53 +0300

    Update button.blade.php

--- src/views/components/button.blade.php
+++ src/views/components/button.blade.php
@@ -32,7 +32,7 @@
             <div class="flex items-center justify-start text-start">
                 <svg class="w-5 h-5 rtl:mr-1 ltr:ml-1 ltr:mr-2 rtl:ml-2 text-white animate-spin 
                 {{ $color == 'none' ? 'text-black dark:text-white' : 'text-white' }}
-                " xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" wire:loading>
+                " xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" wire:loading {{ $attributes->whereStartsWith('wire:target') }}>
                     <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
                     <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
                 </svg>
@@ -49,9 +49,9 @@
             <h1 class="font-bold" wire:loading.class="hidden">
                 {{ $slot }}
             </h1>
-            <svg class="w-5 h-5 text-black rtl:mr-1 ltr:ml-1 ltr:mr-2 rtl:ml-2 dark:text-white animate-spin
+            <svg class="w-5 h-5 text-black dark:text-white animate-spin
             {{ $color == 'none' ? 'text-black dark:text-white' : 'text-white' }}
-            " xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" wire:loading>
+            " xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" wire:loading {{ $attributes->whereStartsWith('wire:target') }}>
                 <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
                 <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
             </svg>
